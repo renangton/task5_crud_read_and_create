4d1cb20c2b81e422fcf85cf9299644812d09ee79
renangton <puccho0427@gmail.com>
2022-02-21 00:15:43 +0900

    mapperを編集

--- sql/task5_create_game_table_and_insert_data.sql
+++ sql/task5_create_game_table_and_insert_data.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS games;
 
 CREATE TABLE games
 (
-	id int NOT NULL,
+	id int unsigned AUTO_INCREMENT,
 	name VARCHAR(20) NOT NULL,
 	genre VARCHAR(20) NOT NULL,
 	price int(10) NOT NULL,
@@ -10,45 +10,48 @@ CREATE TABLE games
 	PRIMARY KEY(id)
 );
 
-INSERT INTO games (id, name, genre, price) VALUES (1, "R6E", "FPS", 6600);
-INSERT INTO games (id, name, genre, price) VALUES (2, "Apex", "FPS", 0);
-INSERT INTO games (id, name, genre, price) VALUES (3, "Tarcov", "FPS", 14500);
-INSERT INTO games (id, name, genre, price) VALUES (4, "WITCHER3", "ARPG", 6300);
-INSERT INTO games (id, name, genre, price) VALUES (5, "Spider-Man MM", "OWAA", 6500);
-INSERT INTO games (id, name, genre, price) VALUES (6, "BIOHAZARD8", "Horror", 7200);
-INSERT INTO games (id, name, genre, price) VALUES (7, "POKEMON", "RPG", 6600);
-INSERT INTO games (id, name, genre, price) VALUES (8, "Bomberman", "Action", 1200);
-INSERT INTO games (id, name, genre, price) VALUES (9, "DARKSOULS", "ARPG", 4200);
-INSERT INTO games (id, name, genre, price) VALUES (10, "Among Us", "Party", 0);
+INSERT INTO games (name, genre, price) VALUES ("R6E", "FPS", 6600);
+INSERT INTO games (name, genre, price) VALUES ("Apex", "FPS", 0);
+INSERT INTO games (name, genre, price) VALUES ("Tarcov", "FPS", 14500);
+INSERT INTO games (name, genre, price) VALUES ("WITCHER3", "ARPG", 6300);
+INSERT INTO games (name, genre, price) VALUES ("Spider-Man MM", "OWAA", 6500);
+INSERT INTO games (name, genre, price) VALUES ("BIOHAZARD8", "Horror", 7200);
+INSERT INTO games (name, genre, price) VALUES ("POKEMON", "RPG", 6600);
+INSERT INTO games (name, genre, price) VALUES ("Bomberman", "Action", 1200);
+INSERT INTO games (name, genre, price) VALUES ("DARKSOULS", "ARPG", 4200);
+INSERT INTO games (name, genre, price) VALUES ("Among Us", "Party", 0);
+INSERT INTO games (name, genre, price) VALUES ("MOTHER3", "RPG", 26000);
 
 DROP TABLE IF EXISTS platforms;
 
 CREATE TABLE platforms
 (
-	id int NOT NULL,
+	id int unsigned AUTO_INCREMENT,
 	platform VARCHAR(20) NOT NULL,
 	
 	PRIMARY KEY(id)
 );
 
-INSERT INTO platforms (id, platform) VALUES (1, "PS4");
-INSERT INTO platforms (id, platform) VALUES (2, "PS5");
-INSERT INTO platforms (id, platform) VALUES (3, "SWITCH");
-INSERT INTO platforms (id, platform) VALUES (4, "Steam");
-INSERT INTO platforms (id, platform) VALUES (5, "Origin");
-INSERT INTO platforms (id, platform) VALUES (6, "UBI");
-INSERT INTO platforms (id, platform) VALUES (7, "iOS");
-INSERT INTO platforms (id, platform) VALUES (8, "PC");
+INSERT INTO platforms (platform) VALUES ("PS4");
+INSERT INTO platforms (platform) VALUES ("PS5");
+INSERT INTO platforms (platform) VALUES ("SWITCH");
+INSERT INTO platforms (platform) VALUES ("Steam");
+INSERT INTO platforms (platform) VALUES ("Origin");
+INSERT INTO platforms (platform) VALUES ("UBI");
+INSERT INTO platforms (platform) VALUES ("iOS");
+INSERT INTO platforms (platform) VALUES ("PC");
+INSERT INTO platforms (platform) VALUES ("NEOGEO");
 
 DROP TABLE IF EXISTS games_platforms;
 
 CREATE TABLE games_platforms
 (
 	id int unsigned AUTO_INCREMENT,
-	games_id int,
-	platforms_id int,
+	games_id int unsigned,
+	platforms_id int unsigned,
 	
 	PRIMARY KEY(id),
+
 	FOREIGN KEY(games_id) REFERENCES games(id),
 	FOREIGN KEY(platforms_id) REFERENCES platforms(id)
 );
@@ -57,6 +60,9 @@ INSERT INTO games_platforms (games_id, platforms_id) VALUES (1, 6);
 INSERT INTO games_platforms (games_id, platforms_id) VALUES (2, 4);
 INSERT INTO games_platforms (games_id, platforms_id) VALUES (3, 8);
 INSERT INTO games_platforms (games_id, platforms_id) VALUES (4, 5);
+INSERT INTO games_platforms (games_id, platforms_id) VALUES (4, 1);
+INSERT INTO games_platforms (games_id, platforms_id) VALUES (4, 3);
+INSERT INTO games_platforms (games_id, platforms_id) VALUES (4, 4);
 INSERT INTO games_platforms (games_id, platforms_id) VALUES (5, 2);
 INSERT INTO games_platforms (games_id, platforms_id) VALUES (6, 2);
 INSERT INTO games_platforms (games_id, platforms_id) VALUES (7, 3);
