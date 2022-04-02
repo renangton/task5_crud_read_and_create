192ef178914b2a19b4684f65ecacd69847f800cc
小山由人 <koyama@reytech.co.jp>
2022-04-02 20:09:44 +0900

    OrderByについてxmlとenumを修正

--- src/main/java/com/crud_read_and_create/service/OrderBy.java
+++ src/main/java/com/crud_read_and_create/service/OrderBy.java
@@ -1,6 +1,24 @@
 package com.crud_read_and_create.service;
 
+import java.util.Arrays;
+
 public enum OrderBy {
-	ASC, DESC;
+	ASC("ASC"), DESC("DESC");
+
+	private String value;
+
+	OrderBy(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public static OrderBy from(String input) {
+		return Arrays.stream(values())
+				.filter(v -> v.name().equals(input))
+				.findFirst().orElseThrow(RuntimeException::new);
+	}
 
 }
