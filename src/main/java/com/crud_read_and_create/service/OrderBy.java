51f0c21f69c4284f4ecdc0c980900760e6d29c83
小山由人 <koyama@reytech.co.jp>
2022-04-02 20:41:30 +0900

    OrderByをより簡単に扱えるように修正

--- src/main/java/com/crud_read_and_create/service/OrderBy.java
+++ src/main/java/com/crud_read_and_create/service/OrderBy.java
@@ -1,23 +1,15 @@
 package com.crud_read_and_create.service;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Arrays;
 
 public enum OrderBy {
-	ASC("ASC"), DESC("DESC");
-
-	private String value;
-
-	OrderBy(String value) {
-		this.value = value;
-	}
-
-	public String getValue() {
-		return value;
-	}
+	ASC, DESC;
 
 	public static OrderBy from(String input) {
 		return Arrays.stream(values())
-				.filter(v -> v.name().equals(input))
+				.filter(v -> v.name().equals(StringUtils.upperCase(input)))
 				.findFirst().orElseThrow(RuntimeException::new);
 	}
 
