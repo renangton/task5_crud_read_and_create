fba779d2e56d44187bd539ee3e10d11621f878f5
renangton <puccho0427@gmail.com>
2022-05-18 00:38:04 +0900

    PlatformFormのコンストラクタから不要なバリデーションの定義を削除

--- src/main/java/com/crud_read_and_create/form/PlatformForm.java
+++ src/main/java/com/crud_read_and_create/form/PlatformForm.java
@@ -1,34 +1,33 @@
 package com.crud_read_and_create.form;
 
-import javax.validation.constraints.NotBlank;
-
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
+
 public class PlatformForm {
-	private Integer id;
+    private Integer id;
 
-	@Length(max = 20, message = "プラットフォームは20文字以内で入力して下さい。")
-	@NotBlank(message = "プラットフォームが未入力です。")
-	private String platform;
+    @Length(max = 20, message = "プラットフォームは20文字以内で入力して下さい。")
+    @NotBlank(message = "プラットフォームが未入力です。")
+    private String platform;
 
-	public PlatformForm(Integer id,
-			@Length(max = 20, message = "プラットフォームは20文字以内で入力して下さい。") @NotBlank(message = "プラットフォームが未入力です。") String platform) {
-		this.id = id;
-		this.platform = platform;
-	}
+    public PlatformForm(Integer id, String platform) {
+        this.id = id;
+        this.platform = platform;
+    }
 
-	public PlatformForm() {
-	}
+    public PlatformForm() {
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public String getPlatform() {
-		return platform;
-	}
+    public String getPlatform() {
+        return platform;
+    }
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
-	}
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
 }
