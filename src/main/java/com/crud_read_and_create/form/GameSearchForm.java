8a23e3831afaac2378261e535fbd6c7fa4e7cc9e
renangton <puccho0427@gmail.com>
2022-05-18 23:09:41 +0900

    検索に使用するIDの正規表現に誤りがあったため修正

--- src/main/java/com/crud_read_and_create/form/GameSearchForm.java
+++ src/main/java/com/crud_read_and_create/form/GameSearchForm.java
@@ -1,38 +1,37 @@
 package com.crud_read_and_create.form;
 
-import java.util.Objects;
-
 import javax.validation.constraints.Pattern;
+import java.util.Objects;
 
 public class GameSearchForm {
 
-	@Pattern(regexp = "|[1-9]|[1-9][0-9]{0,2}0?", message = "IDは1～1000の範囲の整数で入力してください。")
-	private String id;
+    @Pattern(regexp = "|[1-9][0-9]{0,2}|1000", message = "IDは1～1000の範囲の整数で入力してください。")
+    private String id;
 
-	private String order;
+    private String order;
 
-	public GameSearchForm(String id, String order) {
-		this.id = id;
-		this.order = order;
-	}
+    public GameSearchForm(String id, String order) {
+        this.id = id;
+        this.order = order;
+    }
 
-	public GameSearchForm() {
-	}
+    public GameSearchForm() {
+    }
 
-	public Integer getId() {
-		Integer id = Objects.nonNull(this.id) && !this.id.isEmpty() ? Integer.parseInt(this.id) : null;
-		return id;
-	}
+    public Integer getId() {
+        Integer id = Objects.nonNull(this.id) && !this.id.isEmpty() ? Integer.parseInt(this.id) : null;
+        return id;
+    }
 
-	public String getOrder() {
-		return order;
-	}
+    public String getOrder() {
+        return order;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setOrder(String order) {
-		this.order = order;
-	}
+    public void setOrder(String order) {
+        this.order = order;
+    }
 }
