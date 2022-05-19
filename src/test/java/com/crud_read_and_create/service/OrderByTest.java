8adb92b404810e94a5ceacc491cb7ab8ff79ff59
renangton <puccho0427@gmail.com>
2022-05-20 01:55:01 +0900

    OrderByTestにascとdesc以外の値を渡した時にRuntimeExceptionをthrowすることを確認するケースを追加

--- src/test/java/com/crud_read_and_create/service/OrderByTest.java
+++ src/test/java/com/crud_read_and_create/service/OrderByTest.java
@@ -3,6 +3,7 @@ package com.crud_read_and_create.service;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
 
 class OrderByTest {
 
@@ -14,5 +15,10 @@ class OrderByTest {
     @Test
     void descを渡した時_DESCが返されること() {
         assertThat(OrderBy.from("desc")).isEqualTo(OrderBy.DESC);
+    }
+
+    @Test
+    void ascとdesc以外の値を渡した時_RuntimeExceptionが発生すること() {
+        assertThrows(RuntimeException.class, () -> OrderBy.from("exception"));
     }
 }
