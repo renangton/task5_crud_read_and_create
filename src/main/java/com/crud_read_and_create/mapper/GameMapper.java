ef3e2e38a136b45d9e440167a7e3e65dbad00a1b
renangton <puccho0427@gmail.com>
2022-05-23 22:57:54 +0900

    RepositoryにUPDATEとDELETEの機能を追加

--- src/main/java/com/crud_read_and_create/mapper/GameMapper.java
+++ src/main/java/com/crud_read_and_create/mapper/GameMapper.java
@@ -1,29 +1,39 @@
 package com.crud_read_and_create.mapper;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
 import com.crud_read_and_create.entity.Game;
 import com.crud_read_and_create.entity.GamePlatform;
 import com.crud_read_and_create.entity.Platform;
 import com.crud_read_and_create.service.OrderBy;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface GameMapper {
 
-	public List<Game> findAll(@Param("orderBy") OrderBy orderBy);
+    public List<Game> findAll(@Param("orderBy") OrderBy orderBy);
 
-	public Optional<Game> findById(Integer id);
+    public Optional<Game> findById(Integer id);
 
-	public List<Platform> findPlatform();
+    public List<Platform> findPlatform();
 
-	public Integer createGame(Game game);
+    public Integer createGame(Game game);
 
-	public Integer createGamePlatform(@Param("gamePlatformList") List<GamePlatform> gamePlatformList);
+    public Integer createGamePlatform(@Param("gamePlatformList") List<GamePlatform> gamePlatformList);
 
-	public Integer createPlatform(Platform platformData);
+    public Integer createPlatform(Platform platformData);
 
+    public Integer updateGame(Game game);
+
+    public Integer updateGamePlatform(@Param("gamePlatformList") List<GamePlatform> gamePlatformList);
+
+    public Integer updatePlatform(Platform platformData);
+
+    public Integer deleteGame(Integer id);
+
+    public Integer deleteGamePlatform(Integer id);
+
+    public Integer daletePlatform(Integer id);
 }
--- src/test/java/com/crud_read_and_create/mapper/GameMapperTest.java
+++ src/test/java/com/crud_read_and_create/mapper/GameMapperTest.java
@@ -23,80 +23,80 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class GameMapperTest {
 
-	@Autowired
-	GameMapper gameMapper;
+    @Autowired
+    GameMapper gameMapper;
 
-	@Test
-	@DataSet(value = "games.yml")
-	void 昇順が選択されている時_ゲームを昇順で全件取得できること() {
-		List<Platform> platformList1 = Arrays.asList(new Platform(1, "PS5"));
-		List<Platform> platformList2 = Arrays.asList(new Platform(1, "PS5"), new Platform(2, "Steam"));
-		List<Game> expectedGameList = Arrays.asList(new Game(1, "ELDENRING", "ARPG", 9000, platformList1),
-				new Game(2, "Apex", "FPS", 0, platformList2));
-		List<Game> games = gameMapper.findAll(OrderBy.from("asc"));
-		assertThat(games).hasSize(2).isEqualTo(expectedGameList);
-	}
+    @Test
+    @DataSet(value = "games.yml")
+    void 昇順が選択されている時_ゲームを昇順で全件取得できること() {
+        List<Platform> platformList1 = Arrays.asList(new Platform(1, "PS5"));
+        List<Platform> platformList2 = Arrays.asList(new Platform(1, "PS5"), new Platform(2, "Steam"));
+        List<Game> expectedGameList = Arrays.asList(new Game(1, "ELDENRING", "ARPG", 9000, platformList1),
+                new Game(2, "Apex", "FPS", 0, platformList2));
+        List<Game> games = gameMapper.findAll(OrderBy.from("asc"));
+        assertThat(games).hasSize(2).isEqualTo(expectedGameList);
+    }
 
-	@Test
-	@DataSet(value = "games.yml")
-	void 降順が選択されている時_ゲームを降順で全件取得できること() {
-		List<Platform> platformList1 = Arrays.asList(new Platform(1, "PS5"));
-		List<Platform> platformList2 = Arrays.asList(new Platform(1, "PS5"), new Platform(2, "Steam"));
-		List<Game> expectedGameList = Arrays.asList(new Game(2, "Apex", "FPS", 0, platformList2),
-				new Game(1, "ELDENRING", "ARPG", 9000, platformList1));
-		List<Game> games = gameMapper.findAll(OrderBy.from("desc"));
-		assertThat(games).hasSize(2).isEqualTo(expectedGameList);
-	}
+    @Test
+    @DataSet(value = "games.yml")
+    void 降順が選択されている時_ゲームを降順で全件取得できること() {
+        List<Platform> platformList1 = Arrays.asList(new Platform(1, "PS5"));
+        List<Platform> platformList2 = Arrays.asList(new Platform(1, "PS5"), new Platform(2, "Steam"));
+        List<Game> expectedGameList = Arrays.asList(new Game(2, "Apex", "FPS", 0, platformList2),
+                new Game(1, "ELDENRING", "ARPG", 9000, platformList1));
+        List<Game> games = gameMapper.findAll(OrderBy.from("desc"));
+        assertThat(games).hasSize(2).isEqualTo(expectedGameList);
+    }
 
-	@Test
-	@DataSet(value = "games.yml")
-	void 引数のidに対応したゲームを取得できること() {
-		List<Platform> platformList = Arrays.asList(new Platform(1, "PS5"));
-		Optional<Game> game = gameMapper.findById(1);
-		assertThat(game).contains(new Game(1, "ELDENRING", "ARPG", 9000, platformList));
-	}
+    @Test
+    @DataSet(value = "games.yml")
+    void 引数のidに対応したゲームを取得できること() {
+        List<Platform> platformList = Arrays.asList(new Platform(1, "PS5"));
+        Optional<Game> game = gameMapper.findById(1);
+        assertThat(game).contains(new Game(1, "ELDENRING", "ARPG", 9000, platformList));
+    }
 
-	@Test
-	@DataSet(value = "games.yml")
-	void 引数のidに対応したゲームが存在しない時_空のOptionalを取得すること() {
-		Optional<Game> emptyGame = gameMapper.findById(3);
-		assertThat(emptyGame).isEqualTo(Optional.empty());
-	}
+    @Test
+    @DataSet(value = "games.yml")
+    void 引数のidに対応したゲームが存在しない時_空のOptionalを取得すること() {
+        Optional<Game> emptyGame = gameMapper.findById(3);
+        assertThat(emptyGame).isEqualTo(Optional.empty());
+    }
 
-	@Test
-	@DataSet(value = "games.yml")
-	void プラットフォームを全件取得できること() {
-		List<Platform> platformList = gameMapper.findPlatform();
-		assertThat(platformList).hasSize(2).contains(new Platform(1, "PS5"), new Platform(2, "Steam"));
-	}
+    @Test
+    @DataSet(value = "games.yml")
+    void プラットフォームを全件取得できること() {
+        List<Platform> platformList = gameMapper.findPlatform();
+        assertThat(platformList).hasSize(2).contains(new Platform(1, "PS5"), new Platform(2, "Steam"));
+    }
 
-	@Test
-	@DataSet(value = "empty.yml")
-	@ExpectedDataSet(value = "expectedAfterInsertGames.yml", ignoreCols = "id")
-	void ゲームを登録できること() {
-		gameMapper.createGame(new Game(1, "ELDENRING", "ARPG", 9000));
-	}
+    @Test
+    @DataSet(value = "empty.yml")
+    @ExpectedDataSet(value = "expectedAfterInsertGames.yml", ignoreCols = "id")
+    void ゲームを登録できること() {
+        gameMapper.createGame(new Game(1, "ELDENRING", "ARPG", 9000));
+    }
 
-	@Test
-	@DataSet(value = "gamesPlatformsEmpty.yml")
-	@ExpectedDataSet(value = "expectedAfterInsertGamesPlatforms.yml", ignoreCols = "id")
-	void 中間テーブルを1件登録できること() {
-		List<GamePlatform> gamePlatformList = Arrays.asList(new GamePlatform(1, "1"));
-		gameMapper.createGamePlatform(gamePlatformList);
-	}
+    @Test
+    @DataSet(value = "gamesPlatformsEmpty.yml")
+    @ExpectedDataSet(value = "expectedAfterInsertGamesPlatforms.yml", ignoreCols = "id")
+    void 中間テーブルを1件登録できること() {
+        List<GamePlatform> gamePlatformList = Arrays.asList(new GamePlatform(1, "1"));
+        gameMapper.createGamePlatform(gamePlatformList);
+    }
 
-	@Test
-	@DataSet(value = "gamesPlatformsEmpty.yml")
-	@ExpectedDataSet(value = "expectedAfterInsertGamesPlatformsMulti.yml", ignoreCols = "id")
-	void gamePlatformListが複数件の時_中間テーブルを複数件登録できること() {
-		List<GamePlatform> gamePlatformList = Arrays.asList(new GamePlatform(1, "1"), new GamePlatform(1, "2"));
-		gameMapper.createGamePlatform(gamePlatformList);
-	}
+    @Test
+    @DataSet(value = "gamesPlatformsEmpty.yml")
+    @ExpectedDataSet(value = "expectedAfterInsertGamesPlatformsMulti.yml", ignoreCols = "id")
+    void gamePlatformListが複数件の時_中間テーブルを複数件登録できること() {
+        List<GamePlatform> gamePlatformList = Arrays.asList(new GamePlatform(1, "1"), new GamePlatform(1, "2"));
+        gameMapper.createGamePlatform(gamePlatformList);
+    }
 
-	@Test
-	@DataSet(value = "empty.yml")
-	@ExpectedDataSet(value = "expectedAfterInsertPlatforms.yml", ignoreCols = "id")
-	void プラットフォームを登録できること() {
-		gameMapper.createPlatform(new Platform(1, "PS5"));
-	}
+    @Test
+    @DataSet(value = "empty.yml")
+    @ExpectedDataSet(value = "expectedAfterInsertPlatforms.yml", ignoreCols = "id")
+    void プラットフォームを登録できること() {
+        gameMapper.createPlatform(new Platform(1, "PS5"));
+    }
 }
